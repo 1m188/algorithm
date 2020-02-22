6ae323826d3f87d4bb4903ded6f1f92afb18e869
1m188 <228117330@qq.com>
2020-02-22 10:54:27 +0800

    比nim游戏简单

---
+++ OnlineJudge/LeetCode/第1个进度/303.区域和检索-数组不可变.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=cpp
+ *
+ * [303] 区域和检索 - 数组不可变
+ *
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (52.26%)
+ * Likes:    134
+ * Dislikes: 0
+ * Total Accepted:    28.8K
+ * Total Submissions: 48.1K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n' +
+  '[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 
+ * 示例：
+ * 
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ * 
+ * 
+ */
+
+#include "vector"
+using namespace std;
+
+// @lc code=start
+class NumArray
+{
+private:
+    vector<int> nums;
+
+public:
+    NumArray(vector<int> &nums)
+    {
+        this->nums = nums;
+    }
+
+    int sumRange(int i, int j)
+    {
+        int sum = 0;
+        while (i <= j)
+        {
+            sum += nums[i];
+            i++;
+        }
+        return sum;
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(i,j);
+ */
+// @lc code=end
