6e2ce1869b60b8a275598e6389940248c0b05700
1m188 <228117330@qq.com>
2020-02-28 11:54:15 +0800

    遍历每个节点，判断当前节点为根的子树是否和目标子树相等

---
+++ OnlineJudge/LeetCode/第1个进度/572.另一个树的子树.cpp
@@ -0,0 +1,110 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=cpp
+ *
+ * [572] 另一个树的子树
+ *
+ * https://leetcode-cn.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (43.23%)
+ * Likes:    160
+ * Dislikes: 0
+ * Total Accepted:    14.1K
+ * Total Submissions: 32.6K
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s
+ * 也可以看做它自身的一棵子树。
+ * 
+ * 示例 1:
+ * 给定的树 s:
+ * 
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 给定的树 t：
+ * 
+ * 
+ * ⁠  4 
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ * 
+ * 示例 2:
+ * 给定的树 s：
+ * 
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * ⁠   /
+ * ⁠  0
+ * 
+ * 
+ * 给定的树 t：
+ * 
+ * 
+ * ⁠  4
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 返回 false。
+ * 
+ */
+
+#include "cstddef"
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+private:
+    bool same(TreeNode *s, TreeNode *t)
+    {
+        if (!s && !t)
+        {
+            return true;
+        }
+        else if (s && t)
+        {
+            return s->val == t->val && same(s->left, t->left) && same(s->right, t->right);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+public:
+    bool isSubtree(TreeNode *s, TreeNode *t)
+    {
+        return same(s, t) || (s->left ? isSubtree(s->left, t) : false) || (s->right ? isSubtree(s->right, t) : false);
+    }
+};
+// @lc code=end
