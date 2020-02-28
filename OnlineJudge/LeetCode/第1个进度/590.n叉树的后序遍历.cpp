05ac349ab09920ed7f10afb3ecb60434ee96369e
1m188 <228117330@qq.com>
2020-02-28 14:33:12 +0800

    水题不解释，迭代法和上一题一样

---
+++ OnlineJudge/LeetCode/第1个进度/590.n叉树的后序遍历.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=590 lang=cpp
+ *
+ * [590] N叉树的后序遍历
+ *
+ * https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (72.09%)
+ * Likes:    51
+ * Dislikes: 0
+ * Total Accepted:    16.5K
+ * Total Submissions: 22.9K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]\r'
+ *
+ * 给定一个 N 叉树，返回其节点值的后序遍历。
+ * 
+ * 例如，给定一个 3叉树 :
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 返回其后序遍历: [5,6,3,2,4,1].
+ * 
+ * 
+ * 
+ * 说明: 递归法很简单，你可以使用迭代法完成此题吗?
+ */
+
+#include "vector"
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution
+{
+private:
+    vector<int> postVec;
+    void postFind(Node *root)
+    {
+        if (root)
+        {
+            for (Node *c : root->children)
+            {
+                postFind(c);
+            }
+            postVec.push_back(root->val);
+        }
+    }
+
+public:
+    vector<int> postorder(Node *root)
+    {
+        postFind(root);
+        return postVec;
+    }
+};
+// @lc code=end
