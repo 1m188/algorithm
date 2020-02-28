7ec0384e60d41f8d4e761f599932668e82cba00e
1m188 <228117330@qq.com>
2020-02-28 20:42:59 +0800

    这题目绕了我好久，果然还是最开始的想法太复杂了，分成两个循环好做多了

---
+++ OnlineJudge/LeetCode/第1个进度/645.错误的集合.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=645 lang=cpp
+ *
+ * [645] 错误的集合
+ *
+ * https://leetcode-cn.com/problems/set-mismatch/description/
+ *
+ * algorithms
+ * Easy (40.90%)
+ * Likes:    70
+ * Dislikes: 0
+ * Total Accepted:    10K
+ * Total Submissions: 24.5K
+ * Testcase Example:  '[1,2,2,4]'
+ *
+ * 集合 S 包含从1到 n
+ * 的整数。不幸的是，因为数据错误，导致集合里面某一个元素复制了成了集合里面的另外一个元素的值，导致集合丢失了一个整数并且有一个元素重复。
+ * 
+ * 给定一个数组 nums 代表了集合 S 发生错误后的结果。你的任务是首先寻找到重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: nums = [1,2,2,4]
+ * 输出: [2,3]
+ * 
+ * 
+ * 注意:
+ * 
+ * 
+ * 给定数组的长度范围是 [2, 10000]。
+ * 给定的数组是无序的。
+ * 
+ * 
+ */
+
+#include "vector"
+#include "algorithm"
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> findErrorNums(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<int> res;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                res.push_back(nums[i]);
+                for (int j = i + 1; j < nums.size(); j++)
+                {
+                    nums[j - 1] = nums[j];
+                }
+                nums.back() = res[0];
+                break;
+            }
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != i + 1)
+            {
+                res.push_back(i + 1);
+                break;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
