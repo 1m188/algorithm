6a80af091e327fa546aca2796d5587ad65302a05
1m188 <228117330@qq.com>
2020-03-01 15:10:45 +0800

    本来想做动态的数组增长和减少的，但是不知道出了什么run error

---
+++ OnlineJudge/LeetCode/第1个进度/705.设计哈希集合.cpp
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=705 lang=cpp
+ *
+ * [705] 设计哈希集合
+ *
+ * https://leetcode-cn.com/problems/design-hashset/description/
+ *
+ * algorithms
+ * Easy (55.36%)
+ * Likes:    35
+ * Dislikes: 0
+ * Total Accepted:    10.4K
+ * Total Submissions: 18.8K
+ * Testcase Example:  '["MyHashSet","add","add","contains","contains","add","contains","remove","contains"]\n' +
+  '[[],[1],[2],[1],[3],[2],[2],[2],[2]]'
+ *
+ * 不使用任何内建的哈希表库设计一个哈希集合
+ * 
+ * 具体地说，你的设计应该包含以下的功能
+ * 
+ * 
+ * add(value)：向哈希集合中插入一个值。
+ * contains(value) ：返回哈希集合中是否存在这个值。
+ * remove(value)：将给定值从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * MyHashSet hashSet = new MyHashSet();
+ * hashSet.add(1);         
+ * hashSet.add(2);         
+ * hashSet.contains(1);    // 返回 true
+ * hashSet.contains(3);    // 返回 false (未找到)
+ * hashSet.add(2);          
+ * hashSet.contains(2);    // 返回 true
+ * hashSet.remove(2);          
+ * hashSet.contains(2);    // 返回  false (已经被删除)
+ * 
+ * 
+ * 
+ * 注意：
+ * 
+ * 
+ * 所有的值都在 [0, 1000000]的范围内。
+ * 操作的总数目在[1, 10000]范围内。
+ * 不要使用内建的哈希集合库。
+ * 
+ * 
+ */
+
+#include "vector"
+using namespace std;
+
+// @lc code=start
+class MyHashSet
+{
+private:
+    vector<bool> hvec = vector<bool>(1000000 + 1, false);
+
+public:
+    /** Initialize your data structure here. */
+    MyHashSet()
+    {
+    }
+
+    void add(int key)
+    {
+        hvec[key] = true;
+    }
+
+    void remove(int key)
+    {
+        hvec[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    bool contains(int key)
+    {
+        return hvec[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
+// @lc code=end
