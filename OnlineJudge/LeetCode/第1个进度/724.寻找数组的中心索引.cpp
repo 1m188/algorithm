c0e7a30af728eb51cbd51a52a91a58eb0a47c66a
1m188 <228117330@qq.com>
2020-03-01 20:46:58 +0800

    中间的索引不断从左向右变化，左右的和事先记好然后变化相对量

---
+++ OnlineJudge/LeetCode/第1个进度/724.寻找数组的中心索引.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=724 lang=cpp
+ *
+ * [724] 寻找数组的中心索引
+ *
+ * https://leetcode-cn.com/problems/find-pivot-index/description/
+ *
+ * algorithms
+ * Easy (36.26%)
+ * Likes:    159
+ * Dislikes: 0
+ * Total Accepted:    28.8K
+ * Total Submissions: 79.3K
+ * Testcase Example:  '[1,7,3,6,5,6]'
+ *
+ * 给定一个整数类型的数组 nums，请编写一个能够返回数组“中心索引”的方法。
+ * 
+ * 我们是这样定义数组中心索引的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+ * 
+ * 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: 
+ * nums = [1, 7, 3, 6, 5, 6]
+ * 输出: 3
+ * 解释: 
+ * 索引3 (nums[3] = 6) 的左侧数之和(1 + 7 + 3 = 11)，与右侧数之和(5 + 6 = 11)相等。
+ * 同时, 3 也是第一个符合要求的中心索引。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: 
+ * nums = [1, 2, 3]
+ * 输出: -1
+ * 解释: 
+ * 数组中不存在满足此条件的中心索引。
+ * 
+ * 说明:
+ * 
+ * 
+ * nums 的长度范围为 [0, 10000]。
+ * 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。
+ * 
+ * 
+ */
+
+#include "vector"
+#include "algorithm"
+#include "numeric"
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+        if (nums.size() == 0)
+        {
+            return -1;
+        }
+        else if (nums.size() == 1)
+        {
+            return 0;
+        }
+
+        int sumr = accumulate(nums.begin() + 1, nums.end(), 0);
+        int suml = 0;
+        int index = 0;
+        while (index < nums.size() - 1)
+        {
+            if (sumr == suml)
+            {
+                return index;
+            }
+            else
+            {
+                suml += nums[index];
+                index++;
+                sumr -= nums[index];
+            }
+        }
+        return sumr == suml ? index : -1;
+    }
+};
+// @lc code=end
