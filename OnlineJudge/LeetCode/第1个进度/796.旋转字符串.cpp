9cf2bf874cc721071f6c320946ea31d5a9cac6cb
1m188 <228117330@qq.com>
2020-03-03 19:58:42 +0800

    不断地变换A的首尾字母，然后和B比较

---
+++ OnlineJudge/LeetCode/第1个进度/796.旋转字符串.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=796 lang=cpp
+ *
+ * [796] 旋转字符串
+ *
+ * https://leetcode-cn.com/problems/rotate-string/description/
+ *
+ * algorithms
+ * Easy (49.51%)
+ * Likes:    64
+ * Dislikes: 0
+ * Total Accepted:    8K
+ * Total Submissions: 16.2K
+ * Testcase Example:  '"abcde"\n"cdeab"'
+ *
+ * 给定两个字符串, A 和 B。
+ * 
+ * A 的旋转操作就是将 A 最左边的字符移动到最右边。 例如, 若 A = 'abcde'，在移动一次之后结果就是'bcdea'
+ * 。如果在若干次旋转操作之后，A 能变成B，那么返回True。
+ * 
+ * 
+ * 示例 1:
+ * 输入: A = 'abcde', B = 'cdeab'
+ * 输出: true
+ * 
+ * 示例 2:
+ * 输入: A = 'abcde', B = 'abced'
+ * 输出: false
+ * 
+ * 注意：
+ * 
+ * 
+ * A 和 B 长度不超过 100。
+ * 
+ * 
+ */
+
+#include "string"
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    bool rotateString(string A, string B)
+    {
+        if (A == B)
+        {
+            return true;
+        }
+
+        for (int i = 0; i < A.size(); i++)
+        {
+            A.push_back(A[0]);
+            A.erase(A.begin());
+            if (A == B)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
