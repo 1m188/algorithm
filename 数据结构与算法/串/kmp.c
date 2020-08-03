63401d94c6a0574a85ed1236c77f4dbb49ac3787
1m188 <228117330@qq.com>
2020-08-03 18:12:18 +0800

    nextval

--- 数据结构与算法/串/kmp.c
+++ 数据结构与算法/串/kmp.c
@@ -19,8 +19,20 @@ void get_next(const char *str, int next[], int length)
     }
 }
 
+void get_nextval(const char *str, int nextval[], int length)
+{
+    nextval[0] = -1;
+    for (int i = 1; i < length; i++)
+    {
+        int ti = nextval[i - 1];
+        while (ti >= 0 && str[ti] != str[i - 1])
+            ti = nextval[ti];
+        nextval[i] = str[ti + 1] != str[i - 1] ? ti + 1 : nextval[ti + 1];
+    }
+}
+
 // 普通暴力查找的时间复杂度为O(m*n)，kmp为O(m+n)
-int kmp(const char *str, const char *cmp)
+int kmp(const char *str, const char *cmp, void (*get_next)(const char *, int[], int))
 {
     int cmpLen = strlen(cmp);
     int next[cmpLen];
@@ -45,7 +57,8 @@ int kmp(const char *str, const char *cmp)
 int main(int argc, char *argv[])
 {
     const char *str = "ababcabcacbab", *cmp = "abcac";
-    printf("%d\n", kmp(str, cmp));
+    printf("next kmp: %d\n", kmp(str, cmp, get_next));
+    printf("nextval kmp: %d\n", kmp(str, cmp, get_nextval));
 
     return 0;
 }
