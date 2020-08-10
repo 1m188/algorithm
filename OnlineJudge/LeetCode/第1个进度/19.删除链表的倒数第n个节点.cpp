6cda7caa9d54567bc5c5341acb862d3ce462347b
1m188 <228117330@qq.com>
2020-08-10 22:09:03 +0800

    两个指针走，一个先走n，另一个随后出发

---
+++ OnlineJudge/LeetCode/第1个进度/19.删除链表的倒数第n个节点.cpp
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=cpp
+ *
+ * [19] 删除链表的倒数第N个节点
+ *
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (38.10%)
+ * Likes:    932
+ * Dislikes: 0
+ * Total Accepted:    212.1K
+ * Total Submissions: 541.2K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * 
+ * 示例：
+ * 
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * 
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 
+ * 
+ * 说明：
+ * 
+ * 给定的 n 保证是有效的。
+ * 
+ * 进阶：
+ * 
+ * 你能尝试使用一趟扫描实现吗？
+ * 
+ */
+
+#include "cstddef"
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *hd = new ListNode(0);
+        hd->next = head;
+        head = nullptr;
+
+        ListNode *c = hd, *cc = hd;
+        while (cc->next && n--)
+            cc = cc->next;
+
+        while (cc->next)
+            c = c->next, cc = cc->next;
+        cc = c->next;
+        c->next = cc->next;
+        delete cc;
+        head = hd->next;
+        delete hd;
+        return head;
+    }
+};
+// @lc code=end
