ceb56fcd0e91b847b702161c6551c8d739df115e
1m188 <228117330@qq.com>
2020-08-12 00:09:47 +0800

    两个节点取一个

---
+++ OnlineJudge/LeetCode/第1个进度/328.奇偶链表.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=cpp
+ *
+ * [328] 奇偶链表
+ *
+ * https://leetcode-cn.com/problems/odd-even-linked-list/description/
+ *
+ * algorithms
+ * Medium (61.73%)
+ * Likes:    222
+ * Dislikes: 0
+ * Total Accepted:    50.7K
+ * Total Submissions: 80.6K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ * 
+ * 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 1->3->5->2->4->NULL
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 2->1->3->5->6->4->7->NULL 
+ * 输出: 2->3->6->7->1->5->4->NULL
+ * 
+ * 说明:
+ * 
+ * 
+ * 应当保持奇数节点和偶数节点的相对顺序。
+ * 链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+ * 
+ * 
+ */
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *oddEvenList(ListNode *head)
+    {
+        if (!head || !head->next)
+            return head;
+
+        head = new ListNode(0, head);
+        ListNode *cur = head;
+        ListNode *h = new ListNode();
+        ListNode *c = h;
+        while (cur->next)
+        {
+            cur = cur->next;
+            if (cur->next)
+            {
+                ListNode *t = cur->next;
+                cur->next = t->next;
+                t->next = nullptr;
+                c->next = t;
+                c = c->next;
+            }
+        }
+
+        cur->next = h->next;
+        delete h;
+        c = head;
+        head = head->next;
+        delete c;
+        return head;
+    }
+};
+// @lc code=end
