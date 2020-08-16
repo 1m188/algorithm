ca33338ccdcad6642edcd4a69f25d43d29bd203f
1m188 <228117330@qq.com>
2020-08-16 16:35:28 +0800

    判断回文字符串

--- 一些问题/其他/判断回文.cpp
+++ 一些问题/其他/判断回文.cpp
@@ -1,11 +1,38 @@
-//判断一个字符串是否正着和反着是一样的
+/*
 
-#include "string"
+判断一个字符串是否正着和反着是一样的
+
+*/
+
 #include "algorithm"
+#include "iostream"
+#include "string"
 
 bool judgePalindrome(const std::string &str)
 {
     std::string newstr(str);
-    reverse(newstr.begin(), newstr.end());
+    std::reverse(newstr.begin(), newstr.end());
     return newstr == str;
+}
+
+bool judge(const std::string &str)
+{
+    int left = 0, right = str.size() - 1;
+    while (left < right)
+        if (str[left++] != str[right--])
+            return false;
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << judgePalindrome("asdsa") << '\n'
+              << judgePalindrome("asd") << '\n';
+
+    std::cout << '\n';
+
+    std::cout << judge("asdsa") << '\n'
+              << judge("asd") << '\n';
+
+    return 0;
 }
