e261d5c9082e3c2c61ac06e0430a35f04e6d2697
1m188 <228117330@qq.com>
2020-08-17 12:12:56 +0800

    自然数之和

--- 一些问题/其他/自然数之和.cpp
+++ 一些问题/其他/自然数之和.cpp
@@ -1,16 +1,63 @@
-//一个无序自然数数列，给定一个数M，选取自然数数列中两个数，其中他们的和是M，打印所有这样的自然数对
+/*
+
+一个无序自然数数列，给定一个数M，选取自然数数列中两个数，其中他们的和是M，打印所有这样的自然数对
+
+*/
 
 #include "iostream"
-void printAnd(int *num, int N, int M)
+#include "vector"
+
+template <typename T>
+void quickSort(std::vector<T> &vec, const int left, const int right)
 {
-    for (int i = 0; i < N - 1; i++)
+    if (left >= right)
+        return;
+
+    int low = left, high = right;
+    T tmp = vec[low];
+    while (low < high)
     {
-        for (int j = i + 1; j < N; j++)
-        {
-            if (num[i] + num[j] == M)
-            {
-                std::cout << num[i] << ',' << num[j] << '\n';
-            }
-        }
+        while (low < high && vec[high] >= tmp)
+            high--;
+        if (low < high)
+            vec[low++] = vec[high];
+        while (low < high && vec[low] <= tmp)
+            low++;
+        if (low < high)
+            vec[high--] = vec[low];
     }
+    vec[low] = tmp;
+    quickSort(vec, left, low - 1);
+    quickSort(vec, low + 1, right);
+}
+
+void printAnd(const std::vector<int> &v, const int M)
+{
+    std::vector<int> vec(v);
+    quickSort(vec, 0, vec.size() - 1);
+
+    int left = 0, right = vec.size() - 1;
+    int sum = vec[left] + vec[right];
+    while (left < right && sum != M)
+    {
+        if (sum > M)
+            right--;
+        else
+            left++;
+        sum = vec[left] + vec[right];
+    }
+
+    while (left < right)
+    {
+        std::cout << vec[left] << ' ' << vec[right] << '\n';
+        left++, right--;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    std::vector<int> vec{1, 3, 2, 4, 5, 10, 9, 8, 6, 7};
+    printAnd(vec, 10);
+
+    return 0;
 }
