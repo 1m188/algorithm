5967fb05453bbe313868cce442fd43c8a7e5a4b0
1m188 <228117330@qq.com>
2020-08-28 20:13:01 +0800

    手链样式 暴力

--- 蓝桥杯/第6届/个人赛省赛/C语言A组/7/7.cpp
+++ 蓝桥杯/第6届/个人赛省赛/C语言A组/7/7.cpp
@@ -7,11 +7,32 @@
 
 // 请你提交该整数。不要填写任何多余的内容或说明性的文字。
 
+#include "algorithm"
 #include "iostream"
+#include "string"
+#include "vector"
+
+bool cmp(const std::vector<std::string> &vec, const std::string &str)
+{
+    std::string s = str + str, ss = s;
+    std::reverse(ss.begin(), ss.end());
+    for (const std::string &e : vec)
+        if (s.find(e) != std::string::npos || ss.find(e) != std::string::npos)
+            return false;
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
-    std::cout << 1170;
+    std::vector<std::string> vec;
+    std::string lace = "rrrwwwwyyyyy";
+    vec.push_back(lace);
+    while (std::next_permutation(lace.begin(), lace.end()))
+        if (cmp(vec, lace))
+            vec.push_back(lace);
+
+    // std::cout << 1170;
+    std::cout << vec.size();
 
     return 0;
 }
