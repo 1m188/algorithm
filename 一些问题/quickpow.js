fd1d5030ade644f1b3372f11a8f32e0cc3bea697
1m188 <228117330@qq.com>
2020-08-29 09:55:28 +0800

    快速幂js

---
+++ 一些问题/quickpow.js
@@ -0,0 +1,30 @@
+/*
+
+快速幂
+
+*/
+
+/**
+ * 快速幂计算
+ * @param num 底数
+ * @param pow 指数
+ * @returns 幂计算结果
+ */
+function quickpow(num, pow) {
+    if (pow <= 0)
+        return 1;
+    else if (pow == 1)
+        return num;
+    else {
+        let nnum = quickpow(num, parseInt(pow / 2));
+        return pow % 2 ? nnum * nnum * num : nnum * nnum;
+    }
+}
+
+function main() {
+    console.log(quickpow(3, 3));
+    console.log(quickpow(-5, 5));
+    console.log(quickpow(5, 17));
+}
+
+main();
