73a4c0403e7b4b50acc1a44046cc7c0e0a86a78a
1m188 <228117330@qq.com>
2020-09-01 16:01:54 +0800

    修改某些函数错误，添加非递归遍历

--- 数据结构与算法/树/二叉树.cpp
+++ 数据结构与算法/树/二叉树.cpp
@@ -1,27 +1,18 @@
 #include "functional"
+#include "iostream"
 #include "queue"
+#include "stack"
 
 // 二叉树节点
 template <typename T>
 struct BinaryTreeNode
 {
-    T data = reinterpret_cast<T>(0);
-    BinaryTreeNode<T> *left = nullptr;
-    BinaryTreeNode<T> *right = nullptr;
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode<T>(const T &d = reinterpret_cast<T>(0), BinaryTreeNode<T> *l = nullptr, BinaryTreeNode<T> *r = nullptr) : data(d), left(l), right(r) {}
 };
-
-// 递归创建满二叉树
-template <typename T>
-BinaryTreeNode<T> *CreateBinaryTree(int deep)
-{
-    if (deep <= 0)
-        return nullptr;
-
-    BinaryTreeNode<T> *node = new BinaryTreeNode<T>();
-    node->left = CreateBinaryTree<T>(deep - 1);
-    node->right = CreateBinaryTree<T>(deep - 1);
-    return node;
-}
 
 // 删除二叉树
 template <typename T>
@@ -42,8 +33,8 @@ void Preorder(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode<T
     if (root)
     {
         visit(root);
-        Preorder(root->left);
-        Preorder(root->right);
+        Preorder(root->left, visit);
+        Preorder(root->right, visit);
     }
 }
 
@@ -53,9 +44,9 @@ void Inorder(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode<T>
 {
     if (root)
     {
-        Inorder(root->left);
+        Inorder(root->left, visit);
         visit(root);
-        Inorder(root->right);
+        Inorder(root->right, visit);
     }
 }
 
@@ -65,15 +56,15 @@ void Postorder(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode<
 {
     if (root)
     {
-        Postorder(root->left);
-        Postorder(root->right);
+        Postorder(root->left, visit);
+        Postorder(root->right, visit);
         visit(root);
     }
 }
 
 // 层序遍历
 template <typename T>
-void Layerorder(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode<T> *)> &visit)
+void Levelorder(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode<T> *)> &visit)
 {
     if (!root)
         return;
@@ -90,4 +81,114 @@ void Layerorder(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode
         if (n->right)
             que.push(n->right);
     }
+}
+
+// 非递归先序遍历
+template <typename T>
+void preorder_c(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode<T> *)> &visit)
+{
+    std::stack<BinaryTreeNode<T> *> st;
+    while (root || !st.empty())
+        if (root)
+        {
+            visit(root);
+            st.push(root);
+            root = root->left;
+        }
+        else
+        {
+            root = st.top();
+            st.pop();
+            root = root->right;
+        }
+}
+
+// 非递中归序遍历
+template <typename T>
+void inorder_c(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode<T> *)> &visit)
+{
+    std::stack<BinaryTreeNode<T> *> st;
+    while (root || !st.empty())
+        if (root)
+        {
+            st.push(root);
+            root = root->left;
+        }
+        else
+        {
+            root = st.top();
+            st.pop();
+            visit(root);
+            root = root->right;
+        }
+}
+
+// 非递归后序遍历
+template <typename T>
+void postorder_c(BinaryTreeNode<T> *root, const std::function<void(BinaryTreeNode<T> *)> &visit)
+{
+    std::stack<BinaryTreeNode<T> *> st;
+    BinaryTreeNode<T> *t = nullptr;
+    while (root || !st.empty())
+        if (root)
+        {
+            st.push(root);
+            root = root->left;
+        }
+        else
+        {
+            root = st.top();
+            if (root->right && t != root->right)
+                root = root->right;
+            else
+            {
+                visit(root);
+                st.pop();
+                t = root;
+                root = nullptr;
+            }
+        }
+}
+
+int main(int argc, char *argv[])
+{
+    /*
+
+        1
+       / \
+      2   3
+     /     \
+    4       5
+
+    */
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(1);
+    root->left = new BinaryTreeNode<int>(2);
+    root->right = new BinaryTreeNode<int>(3);
+    root->left->left = new BinaryTreeNode<int>(4);
+    root->right->right = new BinaryTreeNode<int>(5);
+
+    const std::function<void(BinaryTreeNode<int> *)> visit = [](BinaryTreeNode<int> *bn) { std::cout << bn->data << ' '; };
+
+    Preorder(root, visit);
+    std::cout << '\n';
+    Inorder(root, visit);
+    std::cout << '\n';
+    Postorder(root, visit);
+    std::cout << '\n';
+    Levelorder(root, visit);
+    std::cout << '\n';
+
+    std::cout << '\n';
+
+    preorder_c(root, visit);
+    std::cout << '\n';
+    inorder_c(root, visit);
+    std::cout << '\n';
+    postorder_c(root, visit);
+    std::cout << '\n';
+
+    DelBinaryTree(root);
+
+    return 0;
 }
