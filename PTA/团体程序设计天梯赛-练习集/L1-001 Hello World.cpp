bd0a334e4fc8f2c4297a6b8da8e75bfd15524cbf
1m188 <228117330@qq.com>
2022-05-17 11:48:49 +0800

    Hello World!

---
+++ PTA/团体程序设计天梯赛-练习集/L1-001 Hello World.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file L1-001 Hello World.cpp
+ * @date 2022-05-17
+ */
+
+/*
+这道超级简单的题目没有任何输入。
+
+你只需要在一行中输出著名短句“Hello World!”就可以了。
+
+输入样例：
+无
+
+输出样例：
+Hello World!
+
+代码长度限制
+16 KB
+
+时间限制
+400 ms
+
+内存限制
+64 MB
+*/
+
+/* ...... */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cout << "Hello World!";
+    return 0;
+}
