33098dd548377e6723280d0b4087399c6f491e17
1m188 <228117330@qq.com>
2022-05-19 19:35:28 +0800

    简单题

---
+++ PTA/团体程序设计天梯赛-练习集/L1-052 2018我们要赢.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file L1-052 2018我们要赢.cpp
+ * @date 2022-05-19
+ */
+
+/*
+2018年天梯赛的注册邀请码是“2018wmyy”，意思就是“2018我们要赢”。本题就请你用汉语拼音输出这句话。
+
+输入格式：
+本题没有输入。
+
+输出格式：
+在第一行中输出：“2018”；第二行中输出：“wo3 men2 yao4 ying2 !”。
+
+输入样例：
+无
+输出样例：
+2018
+wo3 men2 yao4 ying2 !
+代码长度限制
+16 KB
+时间限制
+400 ms
+内存限制
+64 MB
+*/
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("2018\nwo3 men2 yao4 ying2 !");
+    return 0;
+}
