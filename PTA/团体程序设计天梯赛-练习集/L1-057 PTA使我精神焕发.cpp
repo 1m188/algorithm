91e5098bb0c78a6c365cfa522a1df76973d8f43a
1m188 <228117330@qq.com>
2022-05-19 20:47:40 +0800

    简单题

---
+++ PTA/团体程序设计天梯赛-练习集/L1-057 PTA使我精神焕发.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file L1-057 PTA使我精神焕发.cpp
+ * @date 2022-05-19
+ */
+
+/*
+PTA使我精神焕发.jpg
+以上是湖北经济学院同学的大作。本题就请你用汉语拼音输出这句话。
+
+输入格式：
+本题没有输入。
+
+输出格式：
+在一行中按照样例输出，以惊叹号结尾。
+
+输入样例：
+无
+输出样例：
+PTA shi3 wo3 jing1 shen2 huan4 fa1 !
+代码长度限制
+16 KB
+时间限制
+400 ms
+内存限制
+64 MB
+*/
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("PTA shi3 wo3 jing1 shen2 huan4 fa1 !");
+    return 0;
+}
