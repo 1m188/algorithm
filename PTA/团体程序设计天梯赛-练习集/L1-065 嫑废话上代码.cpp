ff006d2588d0fee4c02d28e290acb685304bcc03
1m188 <228117330@qq.com>
2022-05-20 19:26:27 +0800

    简单题

---
+++ PTA/团体程序设计天梯赛-练习集/L1-065 嫑废话上代码.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file L1-065 嫑废话上代码.cpp
+ * @date 2022-05-20
+ */
+
+/*
+Linux 之父 Linus Torvalds 的名言是：“Talk is cheap. Show me the code.”（嫑废话，上代码）。本题就请你直接在屏幕上输出这句话。
+
+输入格式：
+本题没有输入。
+
+输出格式：
+在一行中输出 Talk is cheap. Show me the code.。
+
+输入样例：
+无
+输出样例：
+Talk is cheap. Show me the code.
+代码长度限制
+16 KB
+时间限制
+400 ms
+内存限制
+64 MB
+*/
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Talk is cheap. Show me the code.");
+    return 0;
+}
