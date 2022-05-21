08fc7c96dc2135c33654aa62a8fbfd3b8a5f40c5
1m188 <228117330@qq.com>
2022-05-21 11:25:03 +0800

    简单题

---
+++ PTA/团体程序设计天梯赛-练习集/L1-073 人与神.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file L1-073 人与神.cpp
+ * @date 2022-05-21
+ */
+
+/*
+跨界大神 L. Peter Deutsch 有一句名言：“To iterate is human, to recurse divine.”（迭代的是人，递归的是神）。本题就请你直接在屏幕上输出这句话。
+
+输入格式：
+本题没有输入。
+
+输出格式：
+在一行中输出 To iterate is human, to recurse divine.。
+
+输入样例：
+无
+输出样例：
+To iterate is human, to recurse divine.
+代码长度限制
+16 KB
+时间限制
+400 ms
+内存限制
+64 MB
+*/
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+    printf("To iterate is human, to recurse divine.");
+
+    return 0;
+}
