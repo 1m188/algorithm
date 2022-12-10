180403feee6213cea3597bba06b8018808193ee6
1m188 <228117330@qq.com>
2022-12-10 15:47:37 +0800

    工程数学里的一些方法，添加差商计算

---
+++ 数据结构与算法/em.py
@@ -0,0 +1,16 @@
+'''
+Engineering Mathematics
+工程数学里的一些计算方法
+教材：数值计算方法（曾金平 主编  湖南大学出版社）
+'''
+
+
+def cs(f: function, points: list[float]) -> float | None:
+    '''
+    递归计算差商
+    '''
+    if len(points) <= 1: return None
+    if len(points) == 2:
+        a, b = points
+        return (f(a) - f(b)) / (a - b)
+    return (cs(f, points[:-1]) - cs(f, points[1:])) / (points[0] - points[-1])
