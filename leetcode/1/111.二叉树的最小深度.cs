a47ce8e1a3815b5c46816c0a42048b675c7a9d3d
1m188 <228117330@qq.com>
2023-06-22 23:36:22 +0800

    递归

---
+++ leetcode/1/111.二叉树的最小深度.cs
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=csharp
+ *
+ * [111] 二叉树的最小深度
+ *
+ * https://leetcode.cn/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (52.14%)
+ * Likes:    1024
+ * Dislikes: 0
+ * Total Accepted:    565.5K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最小深度。
+ * 
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 
+ * 说明：叶子节点是指没有子节点的节点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数的范围在 [0, 10^5] 内
+ * -1000 
+ * 
+ * 
+ */
+
+using System;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public int MinDepth(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        if (root.left != null && root.right != null)
+            return 1 + Math.Min(MinDepth(root.left), MinDepth(root.right));
+        else if (root.left == null && root.right == null)
+            return 1;
+        else
+            return 1 + MinDepth(root.left != null ? root.left : root.right);
+    }
+}
+// @lc code=end
+
