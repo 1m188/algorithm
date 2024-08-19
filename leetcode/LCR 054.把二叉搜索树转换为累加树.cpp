9f7051255aed8cb00b4b7a520166c21f0fb92b73
1m188 <228117330@qq.com>
2024-08-19 11:23:50 +0800

    倒序遍历，逐个修改

---
+++ leetcode/LCR 054.把二叉搜索树转换为累加树.cpp
@@ -0,0 +1,154 @@
+/*
+ * @lc app=leetcode.cn id=LCR 054 lang=cpp
+ * @lcpr version=30204
+ *
+ * [LCR 054] 把二叉搜索树转换为累加树
+ *
+ * https://leetcode.cn/problems/w6cpku/description/
+ *
+ * algorithms
+ * Medium (85.26%)
+ * Likes:    72
+ * Dislikes: 0
+ * Total Accepted:    31.5K
+ * Total Submissions: 37K
+ * Testcase Example:  '[4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]'
+ *
+ * 给定一个二叉搜索树，请将它的每个节点的值替换成树中大于或者等于该节点值的所有节点值之和。
+ *
+ *
+ *
+ * 提醒一下，二叉搜索树满足下列约束条件：
+ *
+ *
+ * 节点的左子树仅包含键 小于 节点键的节点。
+ * 节点的右子树仅包含键 大于 节点键的节点。
+ * 左右子树也必须是二叉搜索树。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+ * 输出：[30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：root = [0,null,1]
+ * 输出：[1,null,1]
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：root = [1,0,2]
+ * 输出：[3,3,2]
+ *
+ *
+ * 示例 4：
+ *
+ * 输入：root = [3,2,4,1]
+ * 输出：[7,9,4,10]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中的节点数介于 0 和 10^4^ 之间。
+ * 每个节点的值介于 -10^4 和 10^4 之间。
+ * 树中的所有值 互不相同 。
+ * 给定的树为二叉搜索树。
+ *
+ *
+ *
+ *
+ * 注意：
+ *
+ *
+ * 本题与主站 538 题相同：
+ * https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+ * 本题与主站 1038
+ * 题相同：https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/
+ *
+ *
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int x = 0;
+
+public:
+    TreeNode *convertBST(TreeNode *root) {
+
+        if (!root) return root;
+
+        root->right = convertBST(root->right);
+        x += root->val;
+        root->val = x;
+        root->left = convertBST(root->left);
+        return root;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,null,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,0,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,4,1]\n
+// @lcpr case=end
+
+ */
