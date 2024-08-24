4817176376fccbc28e9996fe0e3d1ed09a86a4d6
1m188 <228117330@qq.com>
2024-08-24 21:42:07 +0800

    记录两个字符串中每个字符的位置，然后求差求和

---
+++ leetcode/3146.permutation-difference-between-two-strings.cpp
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode.cn id=3146 lang=cpp
+ * @lcpr version=30204
+ *
+ * [3146] 两个字符串的排列差
+ *
+ * https://leetcode.cn/problems/permutation-difference-between-two-strings/description/
+ *
+ * algorithms
+ * Easy (86.34%)
+ * Likes:    12
+ * Dislikes: 0
+ * Total Accepted:    17.2K
+ * Total Submissions: 19.7K
+ * Testcase Example:  '"abc"\n"bac"'
+ *
+ * 给你两个字符串 s 和 t，每个字符串中的字符都不重复，且 t 是 s 的一个排列。
+ *
+ * 排列差 定义为 s 和 t 中每个字符在两个字符串中位置的绝对差值之和。
+ *
+ * 返回 s 和 t 之间的 排列差 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "abc", t = "bac"
+ *
+ * 输出：2
+ *
+ * 解释：
+ *
+ * 对于 s = "abc" 和 t = "bac"，排列差是：
+ *
+ *
+ * "a" 在 s 中的位置与在 t 中的位置之差的绝对值。
+ * "b" 在 s 中的位置与在 t 中的位置之差的绝对值。
+ * "c" 在 s 中的位置与在 t 中的位置之差的绝对值。
+ *
+ *
+ * 即，s 和 t 的排列差等于 |0 - 1| + |2 - 2| + |1 - 0| = 2。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "abcde", t = "edbac"
+ *
+ * 输出：12
+ *
+ * 解释： s 和 t 的排列差等于 |0 - 3| + |1 - 2| + |2 - 4| + |3 - 1| + |4 - 0| = 12。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= s.length <= 26
+ * 每个字符在 s 中最多出现一次。
+ * t 是 s 的一个排列。
+ * s 仅由小写英文字母组成。
+ *
+ *
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+
+        vector<int> vecs(26), vect(26);
+
+        for (int i = 0; i < s.size(); i++)
+            vecs[s[i] - 'a'] = i;
+        for (int i = 0; i < t.size(); i++)
+            vect[t[i] - 'a'] = i;
+
+        int res = 0;
+        for (int i = 0; i < 26; i++)
+            res += abs(vecs[i] - vect[i]);
+        return res;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "abc"\n"bac"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abcde"\n"edbac"\n
+// @lcpr case=end
+
+ */
