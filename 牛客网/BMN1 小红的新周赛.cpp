53c4016128c67de137a05303abb79fa67de16db9
1m188 <228117330@qq.com>
2024-11-10 17:06:28 +0800

    输入输出

---
+++ 牛客网/BMN1 小红的新周赛.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int A, B, C, D, E, F;
+    cin >> A >> B >> C >> D >> E >> F;
+
+    cout << A + B + C + D + E + F;
+
+    return 0;
+}
+// 64 位输出请用 printf("%lld")
