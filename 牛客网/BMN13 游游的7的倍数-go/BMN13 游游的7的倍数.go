c934b4075fb1d1e6d1679c89c1303e2a0926a4d9
1m188 <228117330@qq.com>
2024-11-10 18:07:40 +0800

    fix

--- 牛客网/BMN13 游游的7的倍数-go/BMN13 游游的7的倍数.go
+++ 牛客网/BMN13 游游的7的倍数-go/BMN13 游游的7的倍数.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 )
 
 func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
 	x := 0
-	fmt.Scan(&x)
+	fmt.Fscan(in, &x)
 
 	str := strconv.Itoa(x)
 	for i := 0; i <= len(str); i++ {
 		for n := '0'; n <= '9'; n++ {
 			xx := str[:i] + string(n) + str[i:]
-			tmp, _ := strconv.Atoi(xx)
+			tmp, _ := strconv.ParseInt(xx, 10, 64)
 			if tmp%7 == 0 {
-				fmt.Println(xx) // 原来这里不是输出数字，而是输出加了数的字符串，这意味着可以出现前导零...
+				fmt.Fprintln(out, xx) // 原来这里不是输出数字，而是输出加了数的字符串，这意味着可以出现前导零...
 				return
 			}
 		}
