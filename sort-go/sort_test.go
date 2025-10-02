0222731bff8b666feffe1d4c66460d2ed13192b9
1m188 <228117330@qq.com>
2025-10-02 14:27:43 +0800

    重构：优化排序测试用例实现

--- sort-go/sort_test.go
+++ sort-go/sort_test.go
@@ -4,72 +4,34 @@ import (
 	"testing"
 )
 
-func TestInsertSort(t *testing.T) {
+func testSort(t *testing.T, sortFunc func([]int, func(i, j int) bool)) {
 	arr := []int{5, 4, 3, 2, 1}
 	target := []int{1, 2, 3, 4, 5}
-	InsertSort(arr, func(i, j int) bool { return arr[i] < arr[j] })
-	f := true
+	sortFunc(arr, func(i, j int) bool { return arr[i] < arr[j] })
 	for i := range arr {
 		if arr[i] != target[i] {
-			f = false
+			t.Errorf("sort failed, arr = %v, target = %v", arr, target)
+			break
 		}
-	}
-	if !f {
-		t.Errorf("InsertSort failed, arr = %v, target = %v", arr, target)
 	}
 
 	arr = []int{10, 1, 9, 2, 4, 5, 8, 6, 3, 7}
 	target = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	InsertSort(arr, func(i, j int) bool { return arr[i] < arr[j] })
-	f = true
+	sortFunc(arr, func(i, j int) bool { return arr[i] < arr[j] })
 	for i := range arr {
 		if arr[i] != target[i] {
-			f = false
+			t.Errorf("sort failed, arr = %v, target = %v", arr, target)
+			break
 		}
-	}
-	if !f {
-		t.Errorf("InsertSort failed, arr = %v, target = %v", arr, target)
 	}
 
 	arr = []int{}
 	target = []int{}
-	InsertSort(arr, func(i, j int) bool { return arr[i] < arr[j] })
+	sortFunc(arr, func(i, j int) bool { return arr[i] < arr[j] })
 	if len(arr) != 0 {
-		t.Errorf("InsertSort failed, arr = %v, target = %v", arr, target)
+		t.Errorf("sort failed, arr = %v, target = %v", arr, target)
 	}
 }
 
-func TestShellSort(t *testing.T) {
-	arr := []int{5, 4, 3, 2, 1}
-	target := []int{1, 2, 3, 4, 5}
-	ShellSort(arr, func(i, j int) bool { return arr[i] < arr[j] })
-	f := true
-	for i := range arr {
-		if arr[i] != target[i] {
-			f = false
-		}
-	}
-	if !f {
-		t.Errorf("ShellSort failed, arr = %v, target = %v", arr, target)
-	}
-
-	arr = []int{10, 1, 9, 2, 4, 5, 8, 6, 3, 7}
-	target = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	ShellSort(arr, func(i, j int) bool { return arr[i] < arr[j] })
-	f = true
-	for i := range arr {
-		if arr[i] != target[i] {
-			f = false
-		}
-	}
-	if !f {
-		t.Errorf("ShellSort failed, arr = %v, target = %v", arr, target)
-	}
-
-	arr = []int{}
-	target = []int{}
-	ShellSort(arr, func(i, j int) bool { return arr[i] < arr[j] })
-	if len(arr) != 0 {
-		t.Errorf("ShellSort failed, arr = %v, target = %v", arr, target)
-	}
-}
+func TestInsertSort(t *testing.T) { testSort(t, InsertSort) }
+func TestShellSort(t *testing.T)  { testSort(t, ShellSort) }
