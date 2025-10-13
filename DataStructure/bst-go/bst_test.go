aa1775b6ddd9606dfbae209524f45affd4180ad1
1m188 <228117330@qq.com>
2025-10-13 10:43:08 +0800

    新增：实现二叉查找树测试用例及查找方法接口

--- DataStructure/bst-go/bst_test.go
+++ DataStructure/bst-go/bst_test.go
@@ -3,3 +3,23 @@ package bstgo
 import (
 	"testing"
 )
+
+func find[T comparable](root *BSTNode[T]) []T {
+	return nil
+}
+
+func TestNew(t *testing.T) {
+
+}
+
+func TestInsert(t *testing.T) {
+
+}
+
+func TestRemove(t *testing.T) {
+
+}
+
+func TestFind(t *testing.T) {
+
+}
