3dc3dc9e9b16aa66c1536da982390d598bf99fc8
1m188 <228117330@qq.com>
2025-10-13 10:35:55 +0800

    新增：实现二叉查找树基本结构及方法接口

--- DataStructure/bst-go/bst.go
+++ DataStructure/bst-go/bst.go
@@ -5,3 +5,30 @@
 */
 
 package bstgo
+
+// 二叉查找树节点定义
+type BSTNode[T comparable] struct {
+	Left  *BSTNode[T] // 左子树
+	Right *BSTNode[T] // 右子树
+	Val   T           // 节点值
+}
+
+// 构建
+func New[T comparable](vals ...T) *BSTNode[T] {
+	return nil
+}
+
+// 插入
+func (node *BSTNode[T]) Insert(val T) *BSTNode[T] {
+	return nil
+}
+
+// 删除
+func (node *BSTNode[T]) Remove(val T) *BSTNode[T] {
+	return nil
+}
+
+// 查找
+func (node *BSTNode[T]) Find(val T) *BSTNode[T] {
+	return nil
+}
