2f6e02533205167604067e55fa76e48b6a2a3d37
1m188 <228117330@qq.com>
2025-10-28 09:45:59 +0800

    优化：新增Ordered类型约束支持BST节点比较

--- DataStructure/bst-go/bst.go
+++ DataStructure/bst-go/bst.go
@@ -6,8 +6,15 @@
 
 package bstgo
 
+type Ordered interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64 |
+		~string
+}
+
 // 二叉查找树节点定义
-type BSTNode[T comparable] struct {
+type BSTNode[T Ordered] struct {
 	Left  *BSTNode[T] // 左子树
 	Right *BSTNode[T] // 右子树
 	Val   T           // 节点值
--- DataStructure/bst-go/bst_test.go
+++ DataStructure/bst-go/bst_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 // 升序返回 bst 里的数值
-func find[T comparable](root *BSTNode[T]) []T {
+func find[T Ordered](root *BSTNode[T]) []T {
 	res := make([]T, 0)
 	var mid func(*BSTNode[T])
 	mid = func(b *BSTNode[T]) {
