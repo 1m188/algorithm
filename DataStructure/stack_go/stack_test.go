d22b8789965b6154db3402a3d84f7e89b02e9d52
1m188 <228117330@qq.com>
2025-12-22 15:08:15 +0800

    新增：实现Go语言栈结构测试用例

---
+++ DataStructure/stack_go/stack_test.go
@@ -0,0 +1,51 @@
+package stackgo
+
+import "testing"
+
+func test(t *testing.T, s Stack[int]) {
+
+	for i := 0; i < 10; i++ {
+		s.Push(&i)
+		if s.Size() != uint64(i+1) {
+			t.Fatal("size is not correct")
+		}
+	}
+
+	for i := 9; i >= 0; i-- {
+		if *s.Top() != i {
+			t.Fatal("top is not correct")
+		}
+		if s.Size() != uint64(i+1) {
+			t.Fatal("size is not correct")
+		}
+		if *s.Pop() != i {
+			t.Fatal("pop is not correct")
+		}
+	}
+
+	if s.Pop() != nil {
+		t.Fatal("pop is not correct")
+	}
+	if s.Top() != nil {
+		t.Fatal("top is not correct")
+	}
+	if s.Size() != 0 {
+		t.Fatal("size is not correct")
+	}
+
+	for i := 0; i < 10; i++ {
+		s.Push(&i)
+	}
+	if s.Size() != 10 {
+		t.Fatal("size is not correct")
+	}
+	s.Clear()
+	if s.Size() != 0 {
+		t.Fatal("size is not correct")
+	}
+}
+
+func TestStack(t *testing.T) {
+	stack := StackSlice[int]{}
+	test(t, &stack)
+}
