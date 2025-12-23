207f56abe369aa6dc27fe2fd777ea10582fa0360
1m188 <228117330@qq.com>
2025-12-23 09:45:31 +0800

    新增：实现基于切片的Go语言泛型栈结构

--- DataStructure/stack_go/stack.go
+++ DataStructure/stack_go/stack.go
@@ -12,3 +12,38 @@ type Stack[T any] interface {
 	Size() uint64 // 栈大小
 	Clear()       // 清空栈
 }
+
+// 基于切片的栈实现
+type StackSlice[T any] struct {
+	data []T
+}
+
+func (s *StackSlice[T]) Push(v *T) {
+	s.data = append(s.data, *v)
+}
+
+func (s *StackSlice[T]) Pop() *T {
+	if len(s.data) == 0 {
+		return nil
+	} else {
+		v := s.data[len(s.data)-1]
+		s.data = s.data[:len(s.data)-1]
+		return &v
+	}
+}
+
+func (s *StackSlice[T]) Top() *T {
+	if len(s.data) == 0 {
+		return nil
+	} else {
+		return &s.data[len(s.data)-1]
+	}
+}
+
+func (s *StackSlice[T]) Size() uint64 {
+	return uint64(len(s.data))
+}
+
+func (s *StackSlice[T]) Clear() {
+	s.data = []T{}
+}
