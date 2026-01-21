0dab7a467a8caa282066808da0b8d1df0570d278
1m188 <228117330@qq.com>
2026-01-21 10:22:51 +0800

    新增：实现线性查找和二分查找算法(Go)

--- DataStructure/search_go/search.go
+++ DataStructure/search_go/search.go
@@ -4,12 +4,30 @@ Golang 实现 */
 
 package searchgo
 
+import "cmp"
+
 // 顺序查找
 func LinearSearch[T comparable](arr []T, target T) int {
+	for i, v := range arr {
+		if v == target {
+			return i
+		}
+	}
 	return -1
 }
 
 // 二分查找
-func BinarySearch[T comparable](arr []T, target T) int {
+func BinarySearch[T cmp.Ordered](arr []T, target T) int {
+	left, right := 0, len(arr)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
 	return -1
 }
