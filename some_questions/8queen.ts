09a1e72a2114b044ce337ae533486a552cdcfc6c
1m188 <228117330@qq.com>
2026-01-30 14:16:37 +0800

    重构：将八皇后问题封装为IIFE

--- some_questions/8queen.ts
+++ some_questions/8queen.ts
@@ -7,57 +7,63 @@
 解决思路：回溯
 */
 
-/**
- * 回溯算法，推广至 N 皇后
- * @param n 棋盘大小
- * @return 求解方案数
- */
-function queenN(n: number): number {
-    if (n <= 0) return 0; // 棋盘大小不合法
-    if (n == 1) return 1; // 棋盘大小为1，只有一种解决方案
 
-    let vec: boolean[][] = Array.from({ length: n }, () => new Array(n).fill(false));
-    let ans: number = 0;
 
-    // 判断当前位置是否可以放置皇后
-    function judge(row: number, col: number): boolean {
-        for (let i = 0; i < n; i++) {
-            if (i != col && vec[row][i]) return false;
-            if (i != row && vec[i][col]) return false;
-        }
-        for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (vec[i][j]) return false;
-        }
-        for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (vec[i][j]) return false;
-        }
-        for (let i = row + 1, j = col - 1; i < n && j >= 0; i++, j--) {
-            if (vec[i][j]) return false;
-        }
-        for (let i = row + 1, j = col + 1; i < n && j < n; i++, j++) {
-            if (vec[i][j]) return false;
-        }
-        return true;
-    }
+(() => {
 
-    // 回溯法
-    function dfs(row: number = 0): void {
-        if (row == n) {
-            ans++;
-            return;
+    /**
+     * 回溯算法，推广至 N 皇后
+     * @param n 棋盘大小
+     * @return 求解方案数
+     */
+    function queenN(n: number): number {
+        if (n <= 0) return 0; // 棋盘大小不合法
+        if (n == 1) return 1; // 棋盘大小为1，只有一种解决方案
+
+        let vec: boolean[][] = Array.from({ length: n }, () => new Array(n).fill(false));
+        let ans: number = 0;
+
+        // 判断当前位置是否可以放置皇后
+        function judge(row: number, col: number): boolean {
+            for (let i = 0; i < n; i++) {
+                if (i != col && vec[row][i]) return false;
+                if (i != row && vec[i][col]) return false;
+            }
+            for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+                if (vec[i][j]) return false;
+            }
+            for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+                if (vec[i][j]) return false;
+            }
+            for (let i = row + 1, j = col - 1; i < n && j >= 0; i++, j--) {
+                if (vec[i][j]) return false;
+            }
+            for (let i = row + 1, j = col + 1; i < n && j < n; i++, j++) {
+                if (vec[i][j]) return false;
+            }
+            return true;
         }
-        for (let i = 0; i < n; i++) {
-            if (judge(row, i)) {
-                vec[row][i] = true;
-                dfs(row + 1);
-                vec[row][i] = false;
+
+        // 回溯法
+        function dfs(row: number = 0): void {
+            if (row == n) {
+                ans++;
+                return;
+            }
+            for (let i = 0; i < n; i++) {
+                if (judge(row, i)) {
+                    vec[row][i] = true;
+                    dfs(row + 1);
+                    vec[row][i] = false;
+                }
             }
         }
+        dfs();
+
+        return ans;
     }
-    dfs();
 
-    return ans;
-}
+    let n: number = 8;
+    console.log(`八皇后问题共有解决方案数目: ${queenN(n)}`);
 
-let n: number = 8;
-console.log(`八皇后问题共有解决方案数目: ${queenN(n)}`);
+})();
