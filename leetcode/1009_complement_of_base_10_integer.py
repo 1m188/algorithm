1d5b779a0f87efa0d5e09ea2d9729541ef0c83e7
1m188 <228117330@qq.com>
2026-03-11 08:36:34 +0800

    新增：实现LeetCode 1009十进制整数的反码算法(Python)

---
+++ leetcode/1009_complement_of_base_10_integer.py
@@ -0,0 +1,88 @@
+#
+# @lc app=leetcode.cn id=1009 lang=python3
+# @lcpr version=30204
+#
+# [1009] 十进制整数的反码
+#
+# https://leetcode.cn/problems/complement-of-base-10-integer/description/
+#
+# algorithms
+# Easy (58.17%)
+# Likes:    120
+# Dislikes: 0
+# Total Accepted:    35.1K
+# Total Submissions: 60.1K
+# Testcase Example:  '5'
+#
+# 每个非负整数 N 都有其二进制表示。例如， 5 可以被表示为二进制 "101"，11 可以用二进制 "1011" 表示，依此类推。注意，除 N = 0
+# 外，任何二进制表示中都不含前导零。
+#
+# 二进制的反码表示是将每个 1 改为 0 且每个 0 变为 1。例如，二进制数 "101" 的二进制反码为 "010"。
+#
+# 给你一个十进制数 N，请你返回其二进制表示的反码所对应的十进制整数。
+#
+#
+#
+#
+#
+#
+# 示例 1：
+#
+# 输入：5
+# 输出：2
+# 解释：5 的二进制表示为 "101"，其二进制反码为 "010"，也就是十进制中的 2 。
+#
+#
+# 示例 2：
+#
+# 输入：7
+# 输出：0
+# 解释：7 的二进制表示为 "111"，其二进制反码为 "000"，也就是十进制中的 0 。
+#
+#
+# 示例 3：
+#
+# 输入：10
+# 输出：5
+# 解释：10 的二进制表示为 "1010"，其二进制反码为 "0101"，也就是十进制中的 5 。
+#
+#
+#
+#
+# 提示：
+#
+#
+# 0 <= N < 10^9
+# 本题与 476：https://leetcode.cn/problems/number-complement/ 相同
+#
+#
+#
+
+
+# @lcpr-template-start
+
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        return int(bin(n)[2:].replace("0", "2").replace("1", "0").replace("2", "1"), 2)
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# 5\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 7\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 10\n
+# @lcpr case=end
+
+#
