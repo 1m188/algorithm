c5c1b0ef76a66b36f0cd9b5dab53c1c6c37710ae
1m188 <228117330@qq.com>
2026-04-17 08:00:02 +0800

    nowcoder/noob62 a 加 b 问题（函数）.py

---
+++ nowcoder/noob62 a 加 b 问题（函数）.py
@@ -0,0 +1,53 @@
+"""
+描述
+你需要编写一个函数，实现整数之间的加法功能。
+
+具体而言，你的函数会接收两个参数：
+a
+,
+b
+(
+−
+1
+0
+9
+≦
+a
+,
+b
+≦
+1
+0
+9
+)
+a,b(−10
+9
+ ≦a,b≦10
+9
+ )，你需要计算出这两个数的和，并将这个结果作为函数的返回值。
+示例1
+输入：
+114,514
+复制
+返回值：
+628
+复制
+说明：
+114 + 514 = 628
+备注：
+"""
+
+
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#
+# 实现求两个参数的和
+# @param Integer1 int整型
+# @param Integer2 int整型
+# @return int整型
+#
+class Solution:
+    def addTwoInteger(self, Integer1: int, Integer2: int) -> int:
+        # write code here
+
+        return Integer1 + Integer2
